a5b58742ade7dcbd6acb8db18e30f569d4d1d82d
kiddo <330797138@qq.com>
2026-07-22 21:51:59 +0800

    deploy

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
+import os
+
 from inventory_web import create_app
+from waitress import serve
 
 
 app = create_app()
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    serve(
+        app,
+        host=os.getenv("WEB_HOST", "0.0.0.0"),
+        port=int(os.getenv("WEB_PORT", "5000")),
+        threads=4,
+    )
